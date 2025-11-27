fa48a6fdf816006bee68c8cbf371fd724efdc59a
root <root@ip-10-42-24-42.ec2.internal>
2025-11-27 17:12:49 +0000

    Add solution for task 2: printing command line arguments

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import sys
-
 if __name__ == "__main__":
-    argv = sys.argv
-    count = len(argv) - 1
+    import sys
+
+    args = sys.argv[1:]
+    count = len(args)
 
     if count == 0:
         print("0 arguments.")
@@ -13,6 +13,5 @@ if __name__ == "__main__":
         else:
             print("{} arguments:".format(count))
 
-        for i in range(1, len(argv)):
-            print("{}: {}".format(i, argv[i]))
-
+        for i in range(count):
+            print("{}: {}".format(i + 1, args[i]))

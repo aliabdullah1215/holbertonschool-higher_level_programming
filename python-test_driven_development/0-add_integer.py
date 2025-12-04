eb024e3020fa72ee422e521b8769d9e1b3c4ae28
Ali Abdullah <aliabdullah1215@icloud.com>
2025-12-04 23:19:58 +0000

    Clean project and remove duplicate file

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -18,9 +18,18 @@ def add_integer(a, b=98):
     Raises:
         TypeError: If a or b is not an integer or float.
     """
+    # Check invalid types
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
+
+    # Reject NaN and Infinity values (float)
+    if isinstance(a, float):
+        if a != a or a == float("inf") or a == -float("inf"):
+            raise TypeError("a must be an integer")
+    if isinstance(b, float):
+        if b != b or b == float("inf") or b == -float("inf"):
+            raise TypeError("b must be an integer")
 
     return int(a) + int(b)

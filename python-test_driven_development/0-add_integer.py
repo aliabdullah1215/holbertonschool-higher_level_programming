eab93199e3dadf70a6783e04214c123534748ee7
Ali Abdullah <root@ip-10-42-129-252.ec2.internal>
2025-12-06 16:44:39 +0000

    Add solution for task 0: add_integer

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -15,9 +15,14 @@ def add_integer(a, b=98):
     The function accepts integers and floats only, and converts
     floats to integers before performing the addition.
     """
+    # NaN check (NaN != NaN)
+    if isinstance(a, float) and a != a:
+        raise TypeError("a must be an integer")
+    if isinstance(b, float) and b != b:
+        raise TypeError("b must be an integer")
+
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 

6509e1d134e5de907f7fc2a0197b082571c2fb4a
Ali Abdullah <root@ip-10-42-129-252.ec2.internal>
2025-12-06 18:20:21 +0000

    Fix doctest and indentation tests

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,33 +1,19 @@
 #!/usr/bin/python3
 """
-Module for printing a formatted name.
-This module defines the say_my_name function, which prints a name after
-validating that its parameters are strings.
+This module contains one function:
+
+say_my_name(first_name, last_name="")
 """
 
 
-def say_my_name(first_name=None, last_name=""):
+def say_my_name(first_name, last_name=""):
     """
-    Print a formatted name: "My name is <first_name> <last_name>".
-
-    Args:
-        first_name (str): The first name.
-        last_name (str): The last name (optional).
-
-    Raises:
-        TypeError: If first_name or last_name is not a string.
+    prints My name is <first name> <last name>
     """
-
-    # Handle missing argument (hidden checker calls say_my_name())
-    if first_name is None:
-        raise TypeError("first_name must be a string")
-
-    # Validate types
-    if not isinstance(first_name, str):
-        raise TypeError("first_name must be a string")
-
-    if not isinstance(last_name, str):
-        raise TypeError("last_name must be a string")
-
+    if type(first_name) is not str:
+        raise TypeError('first_name must be a string')
+        return
+    if type(last_name) is not str:
+        raise TypeError('last_name must be a string')
+        return
     print("My name is {} {}".format(first_name, last_name))
-

80abc82f6b14371a41d81db7d19aa71afd2e6b7c
Ali Abdullah <aliabdullah1215@icloud.com>
2025-12-13 13:48:10 +0000

    Add size property with validation and area method

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,24 +1,41 @@
 #!/usr/bin/python3
+"""This module defines a Square class with controlled size access."""
 
 
 class Square:
-    '''Initialize Class Square
-    '''
+    """This class represents a square with a private size attribute."""
 
     def __init__(self, size=0):
+        """Initialize a new Square instance.
+
+        Args:
+            size (int): The size of the square.
+        """
         self.size = size
 
     @property
     def size(self):
+        """Get the size of the square."""
         return self.__size
 
     @size.setter
-    def size(self, size):
-        if type(size) != int:
+    def size(self, value):
+        """Set the size of the square.
+
+        Args:
+            value (int): The new size of the square.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = value
 
     def area(self):
-        return (self.size * self.size)
+        """Return the area of the square."""
+        return self.__size ** 2
+

873854afa0d3cc273842d3ae8f10ebfea1c16c73
root <root@ip-10-42-223-47.ec2.internal>
2025-12-20 19:21:09 +0000

    Add 7-base_geometry.py, tests/7-base_geometry.txt

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Defines an empty class named BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+
+    def area(self):
+        """Raise an exception indicating area is not implemented."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate that value is a positive integer.
+
+        Args:
+            name (str): The name associated with the value.
+            value (int): The value to validate.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than or equal to 0.
+        """
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")

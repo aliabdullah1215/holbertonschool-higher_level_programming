c00c0abce06cd77356134543c9b16b486debc813
Ali Abdullah <aliabdullah1215@icloud.com>
2026-01-17 00:52:55 +0000

    SQL: list privileges for user_0d_1 and user_0d_2

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- Task: list privileges of MySQL users
+-- This query lists all privileges of user_0d_1 on localhost
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- This query lists all privileges of user_0d_2 on localhost
+SHOW GRANTS FOR 'user_0d_2'@'localhost';

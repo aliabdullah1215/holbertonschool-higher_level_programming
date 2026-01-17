f33d7988408df80650039ba6d05756883a1a2459
Ali Abdullah <aliabdullah1215@icloud.com>
2026-01-17 01:15:14 +0000

    SQL: list all shows with genre IDs or NULL

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,7 @@
+-- Task: list all shows with their genre IDs
+-- This query lists all shows and their genre IDs, displaying NULL if none
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;

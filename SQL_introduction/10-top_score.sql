eb1c6d35aa3054d70a4b595233becaa73af552d1
Ali Abdullah <aliabdullah1215@icloud.com>
2026-01-17 00:27:27 +0000

    SQL: list records ordered by top score

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- Task: list all records ordered by best score
+-- This query lists score and name from second_table ordered by score descending
+SELECT score, name FROM second_table
+ORDER BY score DESC;

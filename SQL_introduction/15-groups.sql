82f842e06c31443728295cf15a2144fe1c4d865d
Ali Abdullah <aliabdullah1215@icloud.com>
2026-01-17 00:35:49 +0000

    SQL: count records grouped by score

---
+++ SQL_introduction/15-groups.sql
@@ -0,0 +1,6 @@
+-- Task: count records by score
+-- This query lists the number of records for each score in second_table
+SELECT score, COUNT(*) AS number
+FROM second_table
+GROUP BY score
+ORDER BY number DESC;

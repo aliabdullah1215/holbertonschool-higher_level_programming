6296496b14dddcd3346a075fae7406f83ad723bd
Ali Abdullah <aliabdullah1215@icloud.com>
2026-01-17 10:48:20 +0000

    task 1: filter states starting with N

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+This script lists all states starting with the letter N
+from a MySQL database.
+"""
+
+import MySQLdb
+import sys
+
+
+def main():
+    """Connects to MySQL and prints states starting with 'N'."""
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user,
+        passwd=password,
+        db=database
+    )
+
+    cursor = db.cursor()
+    cursor.execute(
+        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;"
+    )
+
+    for row in cursor.fetchall():
+        print(row)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()

1975ec23efd054f430df6d227d0cc7491c0c6b76
Ali Abdullah <aliabdullah1215@icloud.com>
2026-01-17 00:16:16 +0000

    SQL: create first_table

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- Task: create table first_table
+-- This query creates the table first_table in the current database if it does not exist
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
